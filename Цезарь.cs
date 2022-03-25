adf2e199570754cf764b4660feb0e6ef359ca1e5
LastDimka <100382841+LastDimka@users.noreply.github.com>
2022-03-25 22:32:56 +0300

    Add files via upload

---
+++ Цезарь.cs
@@ -0,0 +1,27 @@
+﻿namespace Цезарь
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово,которое нужно зашифровать:");
+            string s = Console.ReadLine();
+            string abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";       
+            Console.WriteLine("Укажите длину сдвига");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int a;
+            string code = string.Empty;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int pos;
+                char c = s[i];
+                pos = abc.IndexOf(c);
+                a = pos + len;
+                if (a > 32) a -= 33;
+                code += abc[a];
+            }
+            Console.WriteLine(code);
+            Console.ReadLine();
+        }
+    }
+}
