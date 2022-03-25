03879c4911ba764e6badc6f98400c1bd5ab4ff0f
LastDimka <100382841+LastDimka@users.noreply.github.com>
2022-03-25 23:46:17 +0300

    Add files via upload

---
+++ Факториал.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Факториал
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            long n = Convert.ToInt64(Console.ReadLine());
+            long p = 1;
+            for (int i = 2; i <= n; ++i)
+                p *= i;
+            Console.WriteLine("Результат: " + p);
+            Console.ReadLine();
+        }
+    }
+}
